e9d37fcc263ac853a41950a6364209a07db0ff24
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2020-10-13 23:45:05 +0700

    implement operations

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,73 +1,360 @@
 package com.example.calculator;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editUSD, editEUR, editVND;
+    private TextView textView;
 
-    double usd2eur = 0.84;
-    double usd2vnd = 23178.0;
+    private static final char ADDITION = '+';
+
+    private static final char SUBTRACTION = '-';
+
+    private static final char MULTIPLICATION = '*';
+
+    private static final char DIVISION = '/';
+
+    private List<Double> values;
+
+    private List<Character> operators;
+
+    private String newOperand;
+
+    private DecimalFormat decimalFormat;
+
+    private Button buttonZero;
+
+    private Button buttonOne;
+
+    private Button buttonTwo;
+
+    private Button buttonThree;
+
+    private Button buttonFour;
+
+    private Button buttonFive;
+
+    private Button buttonSix;
+
+    private Button buttonSeven;
+
+    private Button buttonEight;
+
+    private Button buttonNine;
+
+    private Button buttonAdd;
+
+    private Button buttonSubtract;
+
+    private Button buttonMultiply;
+
+    private Button buttonDivide;
+
+    private Button buttonDot;
+
+    private Button buttonEqual;
+
+    private Button buttonClear;
+
+    private boolean isNewOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editUSD = findViewById(R.id.edit_usd);
-        editEUR = findViewById(R.id.edit_eur);
-        editVND = findViewById(R.id.edit_vnd);
+        decimalFormat = new DecimalFormat("#.##########");
+        values = new ArrayList<>();
+        operators = new ArrayList<>();
+        newOperand = "";
+        isNewOperation = true;
 
-        findViewById(R.id.button_clear).setOnClickListener(new View.OnClickListener() {
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/digital-7.ttf");
+        textView = findViewById(R.id.textView);
+        textView.setTypeface(tf);
+
+        textView.setText("");
+
+        buttonZero = findViewById(R.id.buttonZero);
+        buttonOne = findViewById(R.id.buttonOne);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonFour = findViewById(R.id.buttonFour);
+        buttonFive = findViewById(R.id.buttonFive);
+        buttonSix = findViewById(R.id.buttonSix);
+        buttonSeven = findViewById(R.id.buttonSeven);
+        buttonEight = findViewById(R.id.buttonEight);
+        buttonNine = findViewById(R.id.buttonNine);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSubtract = findViewById(R.id.buttonSubtract);
+        buttonMultiply = findViewById(R.id.buttonMultiply);
+        buttonDivide = findViewById(R.id.buttonDivide);
+        buttonDot = findViewById(R.id.buttonDot);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonEqual = findViewById(R.id.buttonEqual);
+
+        buttonDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editUSD.setText("");
-                editEUR.setText("");
-                editVND.setText("");
+                textView.setText(textView.getText() + ".");
             }
         });
 
-        findViewById(R.id.button_convert).setOnClickListener(new View.OnClickListener() {
+        buttonZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    if (editUSD.hasFocus()) {
-                        Log.v("TAG", "Convert USD");
-                        double usd = Double.parseDouble(editUSD.getText().toString());
-                        Log.v("TAG", "USD = " + usd);
-                        double eur = usd * usd2eur;
-                        double vnd = usd * usd2vnd;
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
 
-                        editEUR.setText(String.format("%.2f", eur));
-                        editVND.setText(String.format("%.0f", vnd));
-                    } else if (editEUR.hasFocus()) {
-                        Log.v("TAG", "Convert EUR");
-                        double eur = Double.parseDouble(editEUR.getText().toString());
-                        Log.v("TAG", "EUR = " + eur);
-                        double usd = eur / usd2eur;
-                        double vnd = usd * usd2vnd;
+                newOperand += "0";
+                textView.setText(textView.getText() + "0");
+            }
+        });
 
-                        editUSD.setText(String.format("%.2f", usd));
-                        editVND.setText(String.format("%.0f", vnd));
-                    } else if (editVND.hasFocus()) {
-                        Log.v("TAG", "Convert VND");
-                        double vnd = Double.parseDouble(editVND.getText().toString());
-                        Log.v("TAG", "VND = " + vnd);
-                        double usd = vnd / usd2vnd;
-                        double eur = usd * usd2eur;
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
 
-                        editEUR.setText(String.format("%.2f", eur));
-                        editUSD.setText(String.format("%.2f", usd));
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+                newOperand += "1";
+                textView.setText(textView.getText() + "1");
+            }
+        });
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "2";
+                textView.setText(textView.getText() + "2");
+            }
+        });
+
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "3";
+                textView.setText(textView.getText() + "3");
+            }
+        });
+
+        buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "4";
+                textView.setText(textView.getText() + "4");
+            }
+        });
+
+        buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "5";
+                textView.setText(textView.getText() + "5");
+            }
+        });
+
+        buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "6";
+                textView.setText(textView.getText() + "6");
+            }
+        });
+
+        buttonSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "7";
+                textView.setText(textView.getText() + "7");
+            }
+        });
+
+        buttonEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "8";
+                textView.setText(textView.getText() + "8");
+            }
+        });
+
+        buttonNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNewOperation) {
+                    isNewOperation = true;
+                    textView.setText("");
+                }
+
+                newOperand += "9";
+                textView.setText(textView.getText() + "9");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!newOperand.equals("")) {
+                    values.add(Double.parseDouble(newOperand));
+                    operators.add(ADDITION);
+                    textView.setText(textView.getText().toString() + ADDITION);
+                    newOperand = "";
                 }
             }
         });
+
+        buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!newOperand.equals("")) {
+                    values.add(Double.parseDouble(newOperand));
+                    operators.add(SUBTRACTION);
+                    textView.setText(textView.getText().toString() + SUBTRACTION);
+                    newOperand = "";
+                }
+            }
+        });
+
+        buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!newOperand.equals("")) {
+                    values.add(Double.parseDouble(newOperand));
+                    operators.add(MULTIPLICATION);
+                    textView.setText(textView.getText().toString() + MULTIPLICATION);
+                    newOperand = "";
+                }
+            }
+        });
+
+        buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!newOperand.equals("")) {
+                    values.add(Double.parseDouble(newOperand));
+                    operators.add(DIVISION);
+                    textView.setText(textView.getText().toString() + DIVISION);
+                    newOperand = "";
+                }
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                values.add(Double.parseDouble(newOperand));
+                textView.setText(computeCalculation());
+                values = new ArrayList<>();
+                operators = new ArrayList<>();
+                newOperand = "";
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (textView.getText().length() > 0) {
+                    CharSequence currentText = textView.getText();
+                    textView.setText(currentText.subSequence(0, currentText.length() - 1));
+                } else {
+                    textView.setText("");
+                }
+            }
+        });
+    }
+
+    private String computeCalculation() {
+        isNewOperation = false;
+
+        if (values.size() == 0) {
+            return "";
+        }
+
+        double result = values.get(0);
+
+        if (values.size() == 1) {
+            if (operators.size() > 0) {
+                if (operators.get(0) == ADDITION)
+                    result *= 2;
+                else if (operators.get(0) == SUBTRACTION)
+                    result = 0;
+                else if (operators.get(0) == MULTIPLICATION)
+                    result = result * result;
+                else if (operators.get(0) == DIVISION)
+                    result = 1;
+            }
+
+            return String.valueOf(result);
+        } else {
+            for (int i = 1; i < values.size(); i++) {
+                if (operators.get(i - 1) == ADDITION)
+                    result += values.get(i);
+                else if (operators.get(i - 1) == SUBTRACTION)
+                    result -= values.get(i);
+                else if (operators.get(i - 1) == MULTIPLICATION)
+                    result *= values.get(i);
+                else if (operators.get(i - 1) == DIVISION) {
+                    if (values.get(i) == 0) {
+                        return "Cannot divide by zero";
+                    } else {
+                        result /= values.get(i);
+                    }
+                }
+
+            }
+
+            return String.valueOf(result);
+        }
     }
 }
